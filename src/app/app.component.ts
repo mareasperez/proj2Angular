fca25079bddb088529de4616feae37c9a260fead
maury <mareasperez7@gmail.com>
2019-03-09 04:16:43 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mj-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'miproj2';
+}
